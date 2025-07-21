41a8b4d8669ba0c85ff67784537ca6d720ab4df9
Tanso <tansomctexp@gmail.com>
2025-07-21 11:29:15 +0900

    D3D12 plugin is added.

---
+++ windows_base_plugin_game/wbp_d3d12/include/interfaces/window_d3d12_facade.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+#include <d3d12.h>
+
+#include "wbp_d3d12/include/interfaces/swap_chain_context.h"
+#include "wbp_d3d12/include/interfaces/render_target_context.h"
+#include "wbp_d3d12/include/interfaces/fence_context.h"
+
+namespace wbp_d3d12
+{
+    class ID3D12WindowFacade
+    {
+    public:
+        virtual ~ID3D12WindowFacade() = default;
+
+        virtual void SetSwapChainContext(std::unique_ptr<ISwapChainContext> swapChainContext) = 0;
+        virtual void SetRenderTargetContext(std::unique_ptr<IRenderTargetContext> renderTargetContext) = 0;
+        virtual void SetFenceContext(std::unique_ptr<IFenceContext> fenceContext) = 0;
+
+        virtual void ResetCommand(ID3D12PipelineState *pipelineState) = 0;
+        virtual void CloseCommand() = 0;
+
+        virtual void SetBarrierToRenderTarget() = 0;
+        virtual void SetBarrierToPresent() = 0;
+        virtual void SetRenderTarget(UINT depthStencilIndex) = 0;
+
+        virtual ID3D12CommandAllocator *GetCommandAllocator() = 0;
+        virtual ID3D12GraphicsCommandList *GetCommandList() = 0;
+
+        virtual void ClearViews(const float (&clearColor)[4], UINT depthStencilIndex) = 0;
+        virtual void Present() = 0;
+
+        virtual void WaitForGPU() = 0;
+        virtual void WaitThisFrameForGPU() = 0;
+
+    };
+
+} // namespace wbp_d3d12
